89d7f74e444d602ec840d84852eece28925ac620
Pablo Gonzalez <pabgon0@gmail.com>
2020-09-02 21:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 function findMatching(drivers, string) {
-return drivers.filter (driver => {
-    driver.toLowerCase() === string.toLowerCase()
-  )
+return drivers.filter (driver => driver.toLowerCase() === string.toLowerCase())
 }
